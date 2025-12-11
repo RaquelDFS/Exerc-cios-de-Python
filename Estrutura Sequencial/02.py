9ae7888a5060df592ff1fdba36273e08ccc78c7f
Raquel Fonseca <raquelfonseca.rf@gmail.com>
2025-12-11 13:51:03 -0300

    3 exercicios

---
+++ Estrutura Sequencial/02.py
@@ -0,0 +1,8 @@
+#Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
+n = int(input('Digite um número: '))
+print(f'O número informado foi {n}')
+
+#OU
+print('Digite um numero')
+numero = input()
+print(f'O número informado foi {numero}')
---
+++ Estrutura Sequencial/03.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça dois números e imprima a soma:
+n1 = n2 = 0
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+s = n1 + n2
+print(f'A soma de {n1} e {n2} números é {s}')
